c881ab2cd54718a4fbc768d807c75ce96dc17a1a
Olgatron <olgis.stern@gmail.com>
2014-09-14 10:21:48 +0200

    added tests

---
+++ Tests/Given_Appveryer.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+	[TestFixture]
+    public class Given_Appveryer
+    {
+		[Test]
+		public void HelloWorldTest()
+		{
+			Assert.AreEqual("Hello World","Hello World");
+		}
+    }
+}
